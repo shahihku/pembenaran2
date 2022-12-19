1f138ab1f44a2ebb3ba2702285eea5b38170c86a
shahihku <121048524+shahihku@users.noreply.github.com>
2022-12-20 03:54:03 +0700

    Create haramssh.sh

---
+++ haramssh.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+# ==========================================
+# Color
+# download menu
+cd /root
+wget https://raw.githubusercontent.com/shahihku/pembenaran2/main/menu/add-on.sh
+wget https://raw.githubusercontent.com/shahihku/pembenaran2/main/menu/addssh.sh
+wget https://raw.githubusercontent.com/shahihku/pembenaran2/main/menu/addtj.sh
+wget https://raw.githubusercontent.com/shahihku/pembenaran2/main/menu/addvl.sh
+wget https://raw.githubusercontent.com/shahihku/pembenaran2/main/menu/addvm.sh
+wget https://raw.githubusercontent.com/shahihku/pembenaran2/main/menu/backup.sh
+wget https://raw.githubusercontent.com/shahihku/pembenaran2/main/menu/cek-service.sh
+wget https://raw.githubusercontent.com/shahihku/pembenaran2/main/menu/cektj.sh
+wget https://raw.githubusercontent.com/shahihku/pembenaran2/main/menu/cekvl.sh
+wget https://raw.githubusercontent.com/shahihku/pembenaran2/main/menu/cekvm.sh
+wget https://raw.githubusercontent.com/shahihku/pembenaran2/main/menu/cekssh.sh
+wget https://raw.githubusercontent.com/shahihku/pembenaran2/main/menu/cert.sh
+wget https://raw.githubusercontent.com/shahihku/pembenaran2/main/menu/clear-log.sh
+wget https://raw.githubusercontent.com/shahihku/pembenaran2/main/menu/clear-ram.sh
+wget https://raw.githubusercontent.com/shahihku/pembenaran2/main/menu/delexp.sh
+wget https://raw.githubusercontent.com/shahihku/pembenaran2/main/menu/deltj.sh
+wget https://raw.githubusercontent.com/shahihku/pembenaran2/main/menu/delvl.sh
+wget https://raw.githubusercontent.com/shahihku/pembenaran2/main/menu/delvm.sh
+wget https://raw.githubusercontent.com/shahihku/pembenaran2/main/menu/delssh.sh
+wget https://raw.githubusercontent.com/shahihku/pembenaran2/main/menu/install.sh
+wget https://raw.githubusercontent.com/shahihku/pembenaran2/main/menu/renewtj.sh
+wget https://raw.githubusercontent.com/shahihku/pembenaran2/main/menu/renewvl.sh
+wget https://raw.githubusercontent.com/shahihku/pembenaran2/main/menu/renewvm.sh
+wget https://raw.githubusercontent.com/shahihku/pembenaran2/main/menu/renewssh.sh
+wget https://raw.githubusercontent.com/shahihku/pembenaran2/main/menu/restart.sh
+wget https://raw.githubusercontent.com/shahihku/pembenaran2/main/menu/speedtest.sh
+wget https://raw.githubusercontent.com/shahihku/pembenaran2/main/menu/xp.sh
+wget https://raw.githubusercontent.com/ProcopasTV/nyoabain/main/skc.sh
+chmod +x /root/add-on.sh
+chmod +x /root/addssh.sh
+chmod +x /root/addtj.sh
+chmod +x /root/addvl.sh
+chmod +x /root/addvm.sh
+chmod +x /root/backup.sh
+chmod +x /root/cek-service.sh
+chmod +x /root/cektj.sh
+chmod +x /root/cekvl.sh
+chmod +x /root/cekvm.sh
+chmod +x /root/cekssh.sh
+chmod +x /root/cert.sh
+chmod +x /root/clear-log.sh
+chmod +x /root/clear-ram.sh
+chmod +x /root/delexp.sh
+chmod +x /root/deltj.sh
+chmod +x /root/delvl.sh
+chmod +x /root/delvm.sh
+chmod +x /root/delssh.sh
+chmod +x /root/install.sh
+chmod +x /root/renewtj.sh
+chmod +x /root/renewvl.sh
+chmod +x /root/renewvm.sh
+chmod +x /root/renewssh.sh
+chmod +x /root/restart.sh
+chmod +x /root/speedtest.sh
+chmod +x /root/xp.sh
+chmod +x /root/skc.sh
+cd
